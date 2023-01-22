daa68e139431d3d5993db8029eac889bb3f8863c
lincore81 <gh.20.lbh81@spamgourmet.com>
2023-01-22 20:27:46 +0100

    Figured out the layout, added finishing touches
    - decided on a grid layout for the article
    - added aside content
    - added image+caption
    - added 'social media' links
    - cleaned up the css

---
+++ js/hamburger.js
@@ -0,0 +1,40 @@
+// open/close hamburger menu
+
+const MAX_VW = 720
+let isUnfolded = false
+
+const menu = document.querySelector(`#main-menu`)
+    , button = document.querySelector(`#hamburger-menu-button`)
+    , buttonimg = button.querySelector(`img`)
+
+function toggleMenu(mode) {
+    let shouldUnfold
+    if (mode === `toggle`) {
+        shouldUnfold = !menu.classList.contains(`unfolded`)
+    } else {
+        shouldUnfold = !!mode
+        menu.classList.toggle(`unfolded`)
+        button.classList.toggle(`unfolded`)
+    }
+
+    if (shouldUnfold) {
+        button.setAttribute(`aria-label`, `Close`)
+        buttonimg.setAttribute(`src`, `/assets/close.svg`)
+        menu.classList.add(`unfolded`)
+        button.classList.add(`unfolded`)
+    } else {
+        button.setAttribute(`aria-label`, `Menu`)
+        buttonimg.setAttribute(`src`, `/assets/menu.svg`)
+        menu.classList.remove(`unfolded`)
+        button.classList.remove(`unfolded`)
+    }
+    isUnfolded = shouldUnfold
+}
+
+button.onclick = () => toggleMenu(`toggle`)
+
+window.onresize = () => {
+    if (isUnfolded && window.innerWidth >= MAX_VW) {
+        toggleMenu(false)
+    }
+}
